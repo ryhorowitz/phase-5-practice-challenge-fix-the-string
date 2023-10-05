659a89a41ffc04756e906d2abd020a7d4b57a56d
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-10-05 14:48:48 -0400

    done using a stack

--- lib/fix_the_string.rb
+++ lib/fix_the_string.rb
@@ -1,1 +1,32 @@
-# your code here
+require 'pry'
+# Iterate through the string. Look at char and the next char in pairs.
+# Does are they same char but in different cases?
+# -yes then remove them from the string
+# -no move to the next char and create an new pair to analyze
+#
+# if there is no next char stop iterating
+def fix_the_string(str)
+  stack = []
+
+  str.each_char do |char|
+    # if the stack isn't empty && char != the last char in the stack
+    # && char uppercase == the last char in the stack uppercased
+    # remove the last char in the stack
+    if !stack.empty? && char != stack.last && char.upcase == stack.last.upcase
+      stack.pop
+
+    else
+      stack.push(char)
+    end
+  end
+
+  result = ''
+  result = stack.pop + result until stack.empty?
+  # make result str var
+  # pop off each char from the stack and add it to the result string.
+  #  adding to the string in reverse order
+  result
+end
+
+# answer = fix_the_string('') # 'aAC'
+# puts " answer is #{answer}"
